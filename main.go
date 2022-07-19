5cb2d7e9360f2892e04fb45aa4967daa5e53593e
Ibrahim <ibrahim.13mohamed@gmail.com>
2022-07-19 14:03:17 -0500

    init

--- main.go
+++ main.go
@@ -10,10 +10,16 @@ import (
 	"github.com/gorilla/mux"
 )
 
+
+
+
 func TimeNow(w http.ResponseWriter, r *http.Request) {
 
+	
+
+
 	params := r.URL.Query()["zone"]
-	location, _:= time.LoadLocation(string(params[1]))
+	location, _:= time.LoadLocation(string(params[0]))
 
 	fmt.Fprint(w, "time is", time.Now().In(location))
 	
@@ -40,6 +46,7 @@ func Now(w http.ResponseWriter,  _*http.Request){
 
 }
 
+func getAddress(w http.ResponseWriter, r *http.Request)
 
 
 
@@ -64,3 +71,29 @@ func main() {
 
 
 }
+
+
+
+
+
+// Assignment 2 )  Write  a go program to get the client IP and reflect/echo
+//back to client page ,after this add feature to server so that server could block to a particular client with a message
+
+// : func ReadUserIP(r *http.Request) string {
+//     IPAddress := r.Header.Get("X-Real-Ip")
+//     if IPAddress == "" {
+//         IPAddress = r.Header.Get("X-Forwarded-For")
+//     }
+//     if IPAddress == "" {
+//         IPAddress = r.RemoteAddr
+//     }
+//     return IPAddress
+// }
+// X-Real-Ip - fetches first true IP (if the requests sits behind multiple NAT sources/load balancer)
+
+// X-Forwarded-For - if for some reason X-Real-Ip is blank and does not return response, get from X-Forwarded-For
+
+// Remote Address - last resort (usually won't be reliable as this might be the last ip or if it is a naked http request to server ie no load balancer)
+
+// add feature to store all past access like date and time to access to the server and display a message that  clientIP:xxxxxxxxxxxx 
+//  your last access to server is  Date  :  xxxx/xx/xxxx time :xxxxxxx
